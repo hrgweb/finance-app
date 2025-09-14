d303e44bbb68109e324debfe6ca8a69160f97f72
hrgweb <gamayonhergenroque.web@gmail.com>
2025-09-14 23:04:35 +0800

    add login form

--- src/lib/server/db/helper.ts
+++ src/lib/server/db/helper.ts
@@ -1,14 +1,14 @@
-// import { integer, text } from 'drizzle-orm/sqlite-core';
-// import { sql } from 'drizzle-orm';
+import { integer, text } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
 
-// export function uuid(name: string) {
-// 	return text(name).$defaultFn(() => crypto.randomUUID());
-// }
+export function uuid(name: string) {
+	return text(name).$defaultFn(() => crypto.randomUUID());
+}
 
-// export function timestamps() {
-// 	return {
-// 		createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
-// 		updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull(),
-// 		deletedAt: integer('deleted_at', { mode: 'timestamp' })
-// 	};
-// }
+export function timestamps() {
+	return {
+		createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+		updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull(),
+		deletedAt: integer('deleted_at', { mode: 'timestamp' })
+	};
+}
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,50 +1,50 @@
-// import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
-// import { timestamps, uuid } from './helper';
+import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
+import { timestamps, uuid } from './helper';
 
-// export const users = sqliteTable('users', {
-// 	id: uuid('id').primaryKey(),
-// 	name: text('name').notNull(),
-// 	email: text('email').notNull(),
-// 	password: text('password').notNull(),
-// 	mobileNo: text('mobile_no').notNull(),
-// 	telNo: text('tel_no'),
-// 	address: text('address').notNull(),
-// 	occupation: text('occupation'),
-// 	...timestamps()
-// });
+export const users = sqliteTable('users', {
+	id: uuid('id').primaryKey(),
+	name: text('name').notNull(),
+	email: text('email').notNull(),
+	password: text('password').notNull(),
+	mobileNo: text('mobile_no').notNull(),
+	telNo: text('tel_no'),
+	address: text('address').notNull(),
+	occupation: text('occupation'),
+	...timestamps()
+});
 
-// export const banks = sqliteTable('banks', {
-// 	id: uuid('id').primaryKey(),
-// 	name: text('name').notNull(),
-// 	description: text('description'),
-// 	address: text('address'),
-// 	mobileNo: text('mobile_no'),
-// 	telNo: text('tel_no'),
-// 	...timestamps()
-// });
+export const banks = sqliteTable('banks', {
+	id: uuid('id').primaryKey(),
+	name: text('name').notNull(),
+	description: text('description'),
+	address: text('address'),
+	mobileNo: text('mobile_no'),
+	telNo: text('tel_no'),
+	...timestamps()
+});
 
-// export const loans = sqliteTable('loans', {
-// 	id: uuid('id').primaryKey(),
-// 	userId: uuid('user_id').references(() => users.id),
-// 	bankId: uuid('bank_id').references(() => banks.id),
-// 	principalAmount: integer('principal_amount').notNull(),
-// 	termsInMonths: integer('terms_in_months').notNull(),
-// 	...timestamps()
-// });
+export const loans = sqliteTable('loans', {
+	id: uuid('id').primaryKey(),
+	userId: uuid('user_id').references(() => users.id),
+	bankId: uuid('bank_id').references(() => banks.id),
+	principalAmount: integer('principal_amount').notNull(),
+	termsInMonths: integer('terms_in_months').notNull(),
+	...timestamps()
+});
 
-// export const loanPayments = sqliteTable('loan_payments', {
-// 	id: uuid('id').primaryKey(),
-// 	userId: uuid('user_id').references(() => users.id),
-// 	bankId: uuid('bank_id').references(() => banks.id),
-// 	loanId: uuid('loan_id').references(() => loans.id),
-// 	amount: integer('amount').notNull(),
-// 	...timestamps()
-// });
+export const loanPayments = sqliteTable('loan_payments', {
+	id: uuid('id').primaryKey(),
+	userId: uuid('user_id').references(() => users.id),
+	bankId: uuid('bank_id').references(() => banks.id),
+	loanId: uuid('loan_id').references(() => loans.id),
+	amount: integer('amount').notNull(),
+	...timestamps()
+});
 
-// export const accounts = sqliteTable('accounts', {
-// 	id: uuid('id').primaryKey(),
-// 	userId: uuid('user_id').references(() => users.id),
-// 	initialAmount: integer('initial_amount').notNull(),
-// 	totalAmount: integer('total_amount').notNull(),
-// 	...timestamps()
-// });
+export const accounts = sqliteTable('accounts', {
+	id: uuid('id').primaryKey(),
+	userId: uuid('user_id').references(() => users.id),
+	initialAmount: integer('initial_amount').notNull(),
+	totalAmount: integer('total_amount').notNull(),
+	...timestamps()
+});
